cddc931d972b3025238a130efadf88dfcd2ec65c
bestbug <bestbug@fake.com>
2020-12-12 10:44:26 +0100

    create mocked GET which return a list of id

--- main_test.go
+++ main_test.go
@@ -10,14 +10,30 @@ import (
 func TestGotchiHandler(t *testing.T) {
 	ctx := context.TODO()
 	request := events.APIGatewayProxyRequest{
-		HTTPMethod: "post",
+		HTTPMethod: "POST",
 		Body:       "example_body",
 	}
 	event, err := GotchiStatus(ctx, request)
 	if err != nil {
 		t.Fatalf("Error: %s\n", err.Error())
 	}
-	expectedResponse := "{\"message\":\"Oh hello there, I just recive your request with method post and with this body: 'example_body' path: ''\"}"
+	expectedResponse := "{\"message\":\"Oh hello there, I just recive your request with method POST and with this body: 'example_body' path: ''\"}"
+	if event.Body != expectedResponse {
+		t.Fatalf("Wrong body returned, expected %s but having %s\n", expectedResponse, event.Body)
+	}
+}
+
+func TestGotchiHandlerGET(t *testing.T) {
+	ctx := context.TODO()
+	request := events.APIGatewayProxyRequest{
+		HTTPMethod: "GET",
+		Path:       "/gotchi1",
+	}
+	event, err := GotchiStatus(ctx, request)
+	if err != nil {
+		t.Fatalf("Error: %s\n", err.Error())
+	}
+	expectedResponse := "{\"know_gotchi\":[\"id1\",\"id2\",\"id3\"]}"
 	if event.Body != expectedResponse {
 		t.Fatalf("Wrong body returned, expected %s but having %s\n", expectedResponse, event.Body)
 	}
